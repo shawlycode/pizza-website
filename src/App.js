0f315e44651c7340e7d137dffe89a4133d77125d
shawlycode <orlandoshawly>
2021-06-16 12:18:29 +0000

    add appjs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
+import Navbar from './Navbar';
+import Carousel from './Carousel';
+import About from './About';
+import PizzaStore from './PizzaStore';
+import ContactUs from './ContactUs';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-  <h1>Pizza website</h1>
+     <h1>pizza website</h1>
+     <Navbar/>
+     <Carousel/>
+     <About/>
+     <PizzaStore/>
+     <ContactUs/>
     </div>
   );
 }
