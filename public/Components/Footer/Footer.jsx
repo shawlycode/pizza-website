ee1c5df99d825054b09176b8a8f5bbd396ebbd7f
shawlycode <orlandoshawly>
2021-06-16 13:53:21 +0000

    add footer

---
+++ public/Components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+    return (
+       
+        <footer>
+            <h3>2021 Pizzamania All Rights Reserved</h3>
+        </footer>
+        
+    )
+}
+
+export default Footer
